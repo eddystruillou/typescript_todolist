91c32a8b3f976c4383d7a6408743d4614c60c863
eddystruillou <eddy.s25@hotmail.fr>
2022-04-23 12:10:04 +0200

    Add element in the todoList and display them

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 import { Todo } from './model';
 
 const App:React.FC = () => {
@@ -9,14 +10,20 @@ const App:React.FC = () => {
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+      setTodo("");
+    }
   };
 
-  console.log(todo);
+  console.log("Todos", todos);
 
   return (  
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
